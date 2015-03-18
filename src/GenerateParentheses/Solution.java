672c7a32306fd81151ab0ef3552f4fdfe8764485
green <sylovegreen@gmail.com>
2015-03-18 10:09:11 +0800

    Generate Parentheses

--- src/GenerateParentheses/Solution.java
+++ src/GenerateParentheses/Solution.java
@@ -6,6 +6,27 @@ import java.util.Stack;
 
 public class Solution {
 
+	public ArrayList<String> generateParenthesis2(int n) {
+	    ArrayList<String> res = new ArrayList<String>();
+	    if(n<=0)
+	        return res;
+	    helper(n,n,new String(),res);
+	    return res;
+	}
+	private void helper(int l, int r, String item, ArrayList<String> res)
+	{
+	    if(r<l)
+	        return;
+	    if(l==0 && r==0)
+	    {
+	        res.add(item);
+	    }
+	    if(l>0)
+	        helper(l-1,r,item+"(",res);
+	    if(r>0)
+	        helper(l,r-1,item+")",res);
+	}
+	
 	static int stringCheck(String str,int n){
 		int left=0,right=0,i=0;
 		while(i<str.length()){
